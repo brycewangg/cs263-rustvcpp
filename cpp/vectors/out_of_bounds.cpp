2711d9c849ab4d72f1a1a502a8bd0ab554c38008
Joseph Ng <yuheho7749@gmail.com>
2025-03-03 17:15:33 -0800

    [feat] vector analysis

--- cpp/vectors/out_of_bounds.cpp
+++ cpp/vectors/out_of_bounds.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[]) {
 	std::vector<int> vec = {1, 2, 3};
-	std::cout << vec[5] << std::endl;
+	// std::cout << vec[5] << std::endl;
 	std::cout << vec.at(5) << std::endl;
 	return 0;
 }
--- rust/vectors/out_of_bounds.rs
+++ rust/vectors/out_of_bounds.rs
@@ -1,5 +1,6 @@
 
 fn main() {
     let vec = vec![1, 2, 3];
-    println!("{:?}", vec[5]);
+    // println!("{:}", vec[5]);
+    println!("{:}", vec.get(5).unwrap());
 }
