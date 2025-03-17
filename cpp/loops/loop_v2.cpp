5ed1c95b8b1c081248242d727c383f22c077b0a5
Joseph Ng <yuheho7749@gmail.com>
2025-03-16 20:32:45 -0700

    [fix] force compiler to not eliminate some code

--- cpp/loops/loop_v2.cpp
+++ cpp/loops/loop_v2.cpp
@@ -1,9 +1,11 @@
+#include <iostream>
 #define NUM 1000000000
 
 int main(int argc, char *argv[]) {
-	int x = 1;
+	double x = 1;
 	for (int i = 0; i < NUM; i++) {
 		x += 1;
 	}
+	std::cout << x << std::endl;
 	return 0;
 }
